250b59e47debdb47b592443c14744da78dba900e
BlazerYoo <blazeryoo1921@gmail.com>
2024-07-27 17:14:27 -0400

    update package versions

--- init.sh
+++ init.sh
@@ -42,7 +42,7 @@ echo h5py==3.1.0 >> requirements.txt
 echo humanfriendly==9.1 >> requirements.txt
 echo importlib-metadata==3.4.0 >> requirements.txt
 echo ipykernel==5.4.3 >> requirements.txt
-echo ipython==7.20.0 >> requirements.txt
+echo ipython==7.16.3 >> requirements.txt
 echo ipython-genutils==0.2.0 >> requirements.txt
 echo ipywidgets==7.6.3 >> requirements.txt
 echo jedi==0.18.0 >> requirements.txt
@@ -62,13 +62,13 @@ echo nbconvert==6.0.7 >> requirements.txt
 echo nbformat==5.1.2 >> requirements.txt
 echo nest-asyncio==1.5.1 >> requirements.txt
 echo notebook==6.2.0 >> requirements.txt
-echo numpy==1.20.1 >> requirements.txt
+echo numpy==1.19.5 >> requirements.txt
 echo onnx==1.8.1 >> requirements.txt
 echo onnxruntime==1.6.0 >> requirements.txt
 echo onnxruntime-tools==1.6.0 >> requirements.txt
 echo opencv-python==4.5.1.48 >> requirements.txt
 echo packaging==20.9 >> requirements.txt
-echo pandas==1.2.2 >> requirements.txt
+echo pandas==1.1.5 >> requirements.txt
 echo pandocfilters==1.4.3 >> requirements.txt
 echo parso==0.8.1 >> requirements.txt
 echo pexpect==4.8.0 >> requirements.txt
@@ -89,7 +89,7 @@ echo python-dateutil==2.8.1 >> requirements.txt
 echo pytz==2021.1 >> requirements.txt
 echo pyzmq==22.0.3 >> requirements.txt
 echo qtconsole==5.0.2 >> requirements.txt
-echo scipy==1.6.0 >> requirements.txt
+echo scipy==1.5.4 >> requirements.txt
 echo seaborn==0.11.1 >> requirements.txt
 echo setuptools==53.0.0 >> requirements.txt
 echo six==1.15.0 >> requirements.txt
@@ -101,7 +101,7 @@ echo termcolor==1.1.0 >> requirements.txt
 echo terminado==0.9.2 >> requirements.txt
 echo testpath==0.4.4 >> requirements.txt
 echo tornado==6.1 >> requirements.txt
-echo traitlets==5.0.5 >> requirements.txt
+echo traitlets==4.3.3 >> requirements.txt
 echo typing-extensions==3.7.4.3 >> requirements.txt
 echo wcwidth==0.2.5 >> requirements.txt
 echo webencodings==0.5.1 >> requirements.txt
